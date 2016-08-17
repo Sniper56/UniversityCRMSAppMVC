90d114e74eeeb8bbb78d7fbf13992b09c7b28dbf
Tofael Ahmed <tofaelahmed1756@gmail.com>
2016-08-17 22:15:22 +0600

    teacher load dorpdownlist

---
+++ UniversityCRMSAppWeb/Controllers/TeacherController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityCRMSAppWeb.BLL;
+using UniversityCRMSAppWeb.Models;
+
+namespace UniversityCRMSAppWeb.Controllers
+{
+    public class TeacherController : Controller
+    {
+        //
+        // GET: /Teacher/
+        TeacherManager teacherManager=new TeacherManager();
+        public ActionResult SaveTeacher()
+        {
+            ViewBag.Department = teacherManager.GetAllDepartment();
+            ViewBag.Designation = teacherManager.GetTeacherDesignation();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SaveTeacher(TeacherModel teacher)
+        {
+
+            ViewBag.Department = teacherManager.GetAllDepartment();
+            ViewBag.Designation = teacherManager.GetTeacherDesignation();
+            return View();
+        }
+	}
+}
