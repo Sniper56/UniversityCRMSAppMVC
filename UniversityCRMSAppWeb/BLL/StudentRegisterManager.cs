ffa6f58304518b40319741e326c42cc8afc5457f
Tofael Ahmed <tofaelahmed1756@gmail.com>
2016-08-24 15:08:59 +0600

    studentregistration

---
+++ UniversityCRMSAppWeb/BLL/StudentRegisterManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniversityCRMSAppWeb.DAL;
+using UniversityCRMSAppWeb.Models;
+
+namespace UniversityCRMSAppWeb.BLL
+{
+    public class StudentRegisterManager
+    {
+      StudentRegisterGateway studentRegisterGateway=new StudentRegisterGateway();
+
+        public int SaveStudent(StudentRegisterModel student,string registrationNo)
+        {
+            return studentRegisterGateway.SaveStudent(student, registrationNo);
+        }
+
+        public List<StudentRegisterViewModel> GetAllStudentRegister()
+        {
+            return studentRegisterGateway.GetAllRegisterStudent();
+        }
+
+        public string GetDepartmentCode(int departmentId)
+        {
+          return  studentRegisterGateway.GetDepartmentCode(departmentId);
+        }
+
+        //public int GetNo(StudentRegisterModel student)
+        //{
+        //    return studentRegisterGateway.GetNo(student);
+        //}
+
+        public int GetNo()
+        {
+            return studentRegisterGateway.GetNo();
+        }
+    }
+}
